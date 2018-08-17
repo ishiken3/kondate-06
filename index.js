a20893063e20e6a0d3adbfca7d1467976fb9f805
石原賢一 <kenp.ohana@gmail.com>
2018-08-17 18:23:17 +0900

    added reply

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // -----------------------------------------------------------------------------
+// 定数の設定
+const LINE_CHANNEL_ACCESS_TOKEN = 'MJkFsPtOsd78RtnSaLotsmt+rrnQaFU18w5nOZbcGrGFlxNKym/hK+b8mwusULHLa8wa3jR5RPX7WzwqBtE805GFKebRwxMBjpgsH3zYm5/pBnA24Mgpe14ICsqv1o8EXXrs2Q7VeW1ZbhGZTx9BRAdB04t89/1O/w1cDnyilFU='; // 追加
+
+// -----------------------------------------------------------------------------
 // モジュールのインポート
 var express = require('express');
-var bodyParser = require('body-parser'); // 追加
+var bodyParser = require('body-parser');
+var request = require('request'); // 追加
 var app = express();
 
 
@@ -20,5 +25,27 @@ var server = app.listen(port, function() {
 // ルーター設定
 app.post('/webhook', function(req, res, next){
     res.status(200).end();
-    console.log(req.body);
+    for (var event of req.body.events){
+        if (event.type == 'message' && event.message.text == 'ハロー'){
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+            }
+            var body = {
+                replyToken: event.replyToken,
+                messages: [{
+                    type: 'text',
+                    text: 'こんにちはー'
+                }]
+            }
+            var url = 'https://api.line.me/v2/bot/message/reply';
+            request({
+                url: url,
+                method: 'POST',
+                headers: headers,
+                body: body,
+                json: true
+            });
+        }
+    }
 });
