bfe74dacd03401e59350b4a1d8c0a39d47187462
石原賢一 <kenp.ohana@gmail.com>
2018-08-17 18:30:29 +0900

    added mecabaas-client

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const LINE_CHANNEL_ACCESS_TOKEN = 'MJkFsPtOsd78RtnSaLotsmt+rrnQaFU18w5nOZbcGrGFl
 // モジュールのインポート
 var express = require('express');
 var bodyParser = require('body-parser');
-var request = require('request'); // 追加
+var request = require('request');
+var mecab = require('mecabaas-client'); // 追加
 var app = express();
-
 
 // -----------------------------------------------------------------------------
 // ミドルウェア設定
@@ -26,26 +26,16 @@ var server = app.listen(port, function() {
 app.post('/webhook', function(req, res, next){
     res.status(200).end();
     for (var event of req.body.events){
-        if (event.type == 'message' && event.message.text == 'ハロー'){
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
-            }
-            var body = {
-                replyToken: event.replyToken,
-                messages: [{
-                    type: 'text',
-                    text: 'こんにちはー'
-                }]
-            }
-            var url = 'https://api.line.me/v2/bot/message/reply';
-            request({
-                url: url,
-                method: 'POST',
-                headers: headers,
-                body: body,
-                json: true
-            });
+        if (event.type == 'message' && event.message.text){
+             // Mecabクラウドサービスでメッセージを解析
+            mecab.parse(event.message.text)
+            .then(
+                function(response){
+                      // 解析結果を出力
+                    console.log(response);
+                }
+            );
+
         }
     }
 });
